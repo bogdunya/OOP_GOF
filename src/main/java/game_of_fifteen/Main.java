1ca371375507d1793efdc3c02210b9fa30eff9eb
Shvidky Bogdan <bogdunya@mail.ru>
2024-03-22 12:29:52 +0300

    Коммит ДО модификации

--- src/main/java/game_of_fifteen/Main.java
+++ src/main/java/game_of_fifteen/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GamePanel::new);
     }
-
+    //f
     static class GamePanel extends JFrame {
         private Game game;
         private WidgetFactory widgetFactory;
